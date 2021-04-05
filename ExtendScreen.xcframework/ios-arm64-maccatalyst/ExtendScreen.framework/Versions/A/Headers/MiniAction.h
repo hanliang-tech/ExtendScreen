13aeea2e1214ad3e073fb1e52bc83f424201e414
BirdMichael <birdmichael126@gmail.com>
2021-04-05 23:24:41 +0800

    update `1.0.1`

---
+++ ExtendScreen.xcframework/ios-arm64-maccatalyst/ExtendScreen.framework/Versions/A/Headers/MiniAction.h
@@ -0,0 +1,28 @@
+//
+//  MiniAction.h
+//  MiniBrowser
+//
+//  Created by Eliyar Eziz on 2020/12/1.
+//
+
+#import <Foundation/Foundation.h>
+
+static NSString * _Nonnull const MiniAction_AC_MAIN = @"__AC_MAIN__";
+static NSString * _Nonnull const MiniAction_AC_START_APP = @"__AC_APP__";
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MiniAction : NSObject
+
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, strong) id params;
+@property (nonatomic, copy) NSMutableDictionary *exp;
+@property (nonatomic, copy) NSString *esId;
+@property (nonatomic, copy) NSString *esPkg;
+//
+//
+//- (NSString *)getJosnStr;
+
+@end
+
+NS_ASSUME_NONNULL_END
